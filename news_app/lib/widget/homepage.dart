3625aca891003cd68c5931af74e3cbe163c21f5d
Marzan Islam <marzanislam999@gmail.com>
2022-11-17 19:47:15 +0600

    .

--- news_app/lib/widget/homepage.dart
+++ news_app/lib/widget/homepage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:news_app/model/news_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/widget/const.dart';
@@ -15,136 +16,144 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int pageNo = 1;
+  int currentIndex = 1;
+
   String sortBy = 'popularity';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.search),
-              iconSize: 35,
-              color: Colors.black,
-            ),
-          )
-        ],
-        backgroundColor: Color.fromARGB(255, 230, 181, 35),
-        title: Text(
-          'News App',
-          style: myStyle(30, Colors.black, FontWeight.bold),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.search),
+                iconSize: 35,
+                color: Colors.black,
+              ),
+            )
+          ],
+          backgroundColor: Color.fromARGB(255, 230, 181, 35),
+          title: Text(
+            'News App',
+            style: myStyle(30, Colors.black, FontWeight.bold),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Container(
-          padding: EdgeInsets.all(15),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Color.fromARGB(255, 184, 167, 119),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 15, left: 15),
+        body: Container(
+            padding: EdgeInsets.all(15),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    height: 60,
+                    color: Colors.transparent,
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         ElevatedButton(
-                          onPressed: () {},
-                          child: Text('Prev'),
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  Color.fromARGB(255, 127, 139, 120)),
-                        ),
-                        Container(
-                          height: 25,
-                          width: 25,
-                          color: Colors.transparent,
-                          child: Center(child: Text('1')),
-                        ),
-                        Container(
-                          height: 25,
-                          width: 25,
-                          color: Colors.white,
-                          child: Center(child: Text('2')),
-                        ),
-                        Container(
-                          height: 25,
-                          width: 25,
-                          color: Colors.transparent,
-                          child: Center(child: Text('3')),
-                        ),
-                        Container(
-                          height: 25,
-                          width: 25,
-                          color: Colors.transparent,
-                          child: Center(child: Text('4')),
-                        ),
-                        Container(
-                          height: 25,
-                          width: 25,
-                          color: Colors.transparent,
-                          child: Center(child: Text('5')),
-                        ),
+                            onPressed: () {
+                              if (pageNo > 1) {
+                                pageNo--;
+                                setState(() {});
+                              }
+                            },
+                            child: Text(
+                              'PREV',
+                              style: GoogleFonts.roboto(),
+                            )),
+                        Flexible(
+                            child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: List.generate(
+                              5,
+                              (index) => GestureDetector(
+                                  onTap: () {
+                                    pageNo = (index + 1);
+                                    setState(() {});
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: index == pageNo - 1
+                                              ? Colors.orange
+                                              : Colors.black,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: Text(
+                                      (index + 1).toString(),
+                                      style: GoogleFonts.nunito(
+                                        fontSize: index == pageNo - 1 ? 20 : 12,
+                                        color: index == pageNo - 1
+                                            ? Colors.orange
+                                            : Colors.black,
+                                      ),
+                                    ),
+                                  ))),
+                        )),
                         ElevatedButton(
-                          onPressed: () {},
-                          child: Text('Next'),
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  Color.fromARGB(255, 127, 139, 120)),
-                        ),
+                            onPressed: () {
+                              if (pageNo < 5) {
+                                pageNo++;
+                                setState(() {});
+                              }
+                            },
+                            child: Text(
+                              'NEXT',
+                              style: GoogleFonts.roboto(),
+                            )),
                       ],
                     ),
                   ),
-                ),
-                FutureBuilder<List<Articles>>(
-                  future: CustomeHttp()
-                      .fetchAllNewsData(pageNo: pageNo, sortBy: sortBy),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text('Found Something error');
-                    } else if (snapshot.data == null) {
-                      return Text('No data found!');
-                    }
+                  FutureBuilder<List<Articles>>(
+                    future: CustomeHttp()
+                        .fetchAllNewsData(pageNo: pageNo, sortBy: sortBy),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('Found Something error');
+                      } else if (snapshot.data == null) {
+                        return Text('No data found!');
+                      }
 
-                    return ListView.builder(
-                        physics: NeverScrollableScrollPhysics(),
-                        itemCount: snapshot.data!.length,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            margin: EdgeInsets.only(top: 5),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    width: 4,
-                                    color: Color.fromARGB(255, 230, 181, 35)),
-                                borderRadius: BorderRadius.circular(15)),
-                            child: ListTile(
-                              leading: Image.network(
-                                  '${snapshot.data![index].urlToImage}'),
-                              title: Text(
-                                '${snapshot.data![index].title}',
-                                style: TextStyle(fontWeight: FontWeight.bold),
+                      return ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: snapshot.data!.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin: EdgeInsets.only(
+                                top: 10,
                               ),
-                              subtitle:
-                                  Text('${snapshot.data![index].description}'),
-                            ),
-                          );
-                        });
-                  },
-                )
-              ],
-            ),
-          )),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 4,
+                                      color: Color.fromARGB(255, 230, 181, 35)),
+                                  borderRadius: BorderRadius.circular(15)),
+                              child: ListTile(
+                                leading: Image.network(
+                                    '${snapshot.data![index].urlToImage}'),
+                                title: Text(
+                                  '${snapshot.data![index].title}',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Text(
+                                    '${snapshot.data![index].description}'),
+                              ),
+                            );
+                          });
+                    },
+                  )
+                ],
+              ),
+            )),
+      ),
     );
   }
 }
